302d233eff534dd45052880b63b7d5ed613d6c3e
Luka Hietala <luka.hietala08@gmail.com>
2026-08-12 11:06:46 +0300

    Flask, get scans, websockets

--- db.py
+++ db.py
@@ -16,6 +16,24 @@ def init_db():
         )
     """)
 
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            rfid_id TEXT NOT NULL,
+            name TEXT NOT NULL,
+            status TEXT NOT NULL,
+            created_at TEXT NOT NULL
+        )
+    """)
+
+
+
+    # monta päivää pitää olla töissä
+    # millä aikavälillä? 1.8.2026 - 30.8.2026
+    # kauan per päivä.
+    # kauan vielä pitää tehdä töitä, joka työpäivä lisätään esim 7 tuntia
+    # jonka pitää kiriä
+
     con.commit()
     con.close()
 
@@ -34,6 +52,22 @@ def log_scan(rfid_id, name):
     con.close()
 
     return timestamp
+
+def get_scans(limit=1000):
+    con = sqlite3.connect(DB_NAME)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    cur.execute("""
+        SELECT *
+        FROM scans
+        LIMIT ?
+    """, (limit,))
+
+    rows = cur.fetchall()
+    con.close()
+
+    return [dict(row) for row in rows]
 
 if __name__ == "__main__":
     init_db()
